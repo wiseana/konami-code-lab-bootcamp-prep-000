a49ca063068ead9f66c4d6a50908a7f5b7303aa9
Anastasia Wise <wise.anastasia@gmail.com>
2018-06-17 16:08:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,4 +15,4 @@ const codes = [
 *//input=document.body; index=0;
 *//input.addEventListener('keydown', function onKeyDownHandler(e) {const key = e.key; if (key === codes[index]) {index++; if (index === codes.length) {alert("Hurray!"); return index;}} else {index = 0;}})
 }
-function init3() {input=document.body; input.addEventListener('keydown', function typeKey(e) {let i=0; result=[0]; if (e.key===codes[i]) {i++; result.push (e.key); if (result.length===codes.length){alert("Code complete!"); return result;}} else {i=0; result=[0];}})}
+function init() {input=document.body; input.addEventListener('keydown', function typeKey(e) {let i=0; result=[0]; if (e.key===codes[i]) {i++; result.push (e.key); if (result.length===codes.length){alert("Code complete!"); return result;}} else {i=0; result=[0];}})}
